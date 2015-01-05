d00f6bc91d37bc88b7d965ef07ce9fe9f1cab197
Kartik Kannapur <kartikkannapur@gmail.com>
2015-01-06 00:11:58 +0530

    Create 010.cpp

---
+++ 010.cpp
@@ -0,0 +1,16 @@
+//Program 010.cpp - Manipulation of bases
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	//char myArray[20];
+	char myArray2[20] = "Kartik";
+	cout << myArray2;
+	
+	string myString = "Kartik Kannapur";
+	cout << myString[1];
+
+	
+	return 0;
+}
