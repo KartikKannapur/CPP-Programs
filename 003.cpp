12d39ca1e2fb29b8513d62ed886997b93fe76909
Kartik Kannapur <kartikkannapur@gmail.com>
2015-01-05 23:56:45 +0530

    Create 003.cpp

---
+++ 003.cpp
@@ -0,0 +1,15 @@
+//Program 003.cpp - Function definition and Function call
+
+#include <iostream>
+using namespace std;
+
+int myFunc()
+{
+	cout << "Hello World via a function";
+	return 0;
+}
+
+int main() {
+	myFunc();
+	return 0;
+}
